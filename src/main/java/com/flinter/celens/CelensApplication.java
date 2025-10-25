3902864b6ea1dd9e9bca4293572e0dc14c427499
jhan0121 <jhan0121@gmail.com>
2025-10-25 23:34:01 +0900

    init: project initialize

---
+++ src/main/java/com/flinter/celens/CelensApplication.java
@@ -0,0 +1,13 @@
+package com.flinter.celens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelensApplication.class, args);
+	}
+
+}
